ba7d7c9654fb6a93d052a651fcca4dc342fe0d6b
eslunap19 <estefany.luna@gmail.com>
2020-11-30 05:23:42 -0500

    CREATE GUI usuarios-series
    Se inicio la creación de la interfaz grafica, ya se encuentra realizado el CREATE de usuarios y series.
    
    Nota: aun faltan realizar validaciones  para manejo de excepciones

--- redflix/src/main/java/co/com/G2M2E1/redflix45/Redflix45Application.java
+++ redflix/src/main/java/co/com/G2M2E1/redflix45/Redflix45Application.java
@@ -2,13 +2,25 @@ package co.com.G2M2E1.redflix45;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("co.com.G2M2E1.redflix45")
 public class Redflix45Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Redflix45Application.class, args);
+		//
                // System.out.println("Hello World");
+               new RedflixFrame().setVisible(true);
 	}
+        
+        public static void runSpringServer(String[] args) {
+            /*
+            ConfigurableApplicationContext ctx = new SpringApplicationBuilder(Application.class)
+                    .headless(false).run(args);
+            */
+            SpringApplication.run(Redflix45Application.class, args);
+            //SpringApplication.run(ArtApplication.class, args);
+        }
 
 }
